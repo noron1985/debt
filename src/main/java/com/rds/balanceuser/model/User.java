38785b69f5f0617d8b8bdb04ee103cf95e16c123
noron <ron.desouter@gmail.com>
2019-09-24 11:36:52 +0200

    WIP getmapping creditor and debitor

--- src/main/java/com/rds/balanceuser/model/User.java
+++ src/main/java/com/rds/balanceuser/model/User.java
@@ -1,14 +1,31 @@
 package com.rds.balanceuser.model;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(name = "name")// pour changer le nom par défaut de la variable
     private String name;
+    @Basic(optional = false, fetch = FetchType.EAGER) // contrainte pour null
+    // FetchType Lazy pour un gros volume de donnée
+    @Column(unique = true)
+    // peut produire un erreur qu'on peut catcher et envoyer un message à la personne disant que l'email doit etre unique
     private String mail;
+    @Transient // pas de persistence dans la DB
     private String password;
     private List<Balance> balances;
+
+
+    protected User() {
+    }
+
 
     public User(int id, String name, String mail, String password, List<Balance> balances) {
         this.id = id;
@@ -20,10 +37,6 @@ public class User {
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
